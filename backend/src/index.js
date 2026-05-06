a93a755bb86dfc3b612c10154461d65331b43d58
Đào Đức Anh <20230786@eaut.edu.vn>
2026-05-07 00:31:58 +0700

    Thiết lập server Express

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import pool from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("DB ERROR:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
